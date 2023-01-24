297667cb0a829488df7c1cedda10fc1f5083c166
Yani93 <yani.atek@outlook.fr>
2023-01-24 14:35:02 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 #include <Arduino.h>
 
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+  DHT dht(SENSOR, DHT11);
+  pinMode(LED, OUTPUT);
+
 }
 
 void loop() {
-  // put your code here to run repeatedly
+
+  delay(5000);
+
+  // Initialiser une variable pour stocker les résultats de la mesure
+  float humidity, temperature;
+
+  // Utiliser la bibliothèque DHT sensor pour lire les valeurs d'humidité et de température
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+
+  // Afficher l'humidité relative avec un peu de formatage
+  Serial.print("Humidité relative : ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  // Afficher la température avec un peu de formatage
+  Serial.print("Température : ");
+  Serial.print(temperature);
+  Serial.println("°C");
+
 }
